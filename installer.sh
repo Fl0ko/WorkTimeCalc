65074fb7ff3e2f778aea7950c7eb9901510b2499
Flo <florian.nirschl@freenet.de>
2023-01-19 22:38:24 +0100

    Writing the installer.sh and wtc.sh.

--- installer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl -o /usr/local/bin/wtc.exe https://github.com/Fl0ko/WorkTimeCalc/blob/master/wtc.exe?raw=true
-curl -o /usr/local/bin/wtc.sh https://raw.githubusercontent.com/Fl0ko/WorkTimeCalc/master/wtc.sh
-sudo chown root: /usr/local/bin/wtc.sh
-sudo chmod 755 /usr/local/bin/wtc.sh
